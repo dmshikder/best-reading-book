1f2f8ad603c14e54e7b984a4667793ca216e0357
dmshikder <dmshikder@gmail.com>
2022-04-20 00:33:01 +0600

    searching result completed

--- JS/phone-search.js
+++ JS/phone-search.js
@@ -2,32 +2,35 @@ const loadPhone= (searchText)=>{
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
     .then(Response =>Response.json())
-    .then (data => displayPhone (data.data))
-}
-// loadPhone()
-
-const displayPhone = phones =>{
-    const container = document.getElementById('phones');
-    container.textContent = '';
-    phones.forEach(phone =>{
-        const div= document.createElement('div');
-        div.innerHTML=`
-        <h1>${phone.phone_name} </h1>
-        <button onclick= "loadPhoneDetails('${phone.phone_name}')" > Details </button>
-        `;
-        container.appendChild(div);
-    })
-    
+    .then (data => displaySearchResult (data.data))
 }
 
 // phone db
 const searchPhone = () =>{
-  const searchPhoneText = document.getElementById('search-field').value;
-  loadPhone(searchPhoneText);
-  document.getElementById('search-field').value ='';
+    const searchPhoneText = document.getElementById('search-field').value;
+    loadPhone(searchPhoneText);
+    document.getElementById('search-field').value ='';
+  
+  }
 
-}
-
-const loadPhoneDetails = phoneName =>{
-    console.log(phoneName)
-}
+  const displaySearchResult = phones => {
+      const searchResult = document.getElementById ('search-result');
+      phones.forEach(phone => {
+          const div = document.createElement('div');
+          console.log(phone)
+          div.classList.add('col')
+          div.innerHTML= `
+          <div class="card">
+          <img src="${phone.image}" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">${phone.phone_name}</h5>
+            <p class="card-text"> Brand Name:
+              ${phone.brand}
+            </p>
+            <button> Details </button>
+          </div>
+        </div>
+          `;
+          searchResult.appendChild(div);
+      })
+  }
